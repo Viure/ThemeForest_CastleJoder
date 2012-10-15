e25c16141db718dcef43c646f6f41dc6bc8bb76f
Viure <elviure@gmail.com>
2012-10-16 00:25:18 +0200

    Pagina traida de google drive - copia actualizada

--- js/main.js
+++ js/main.js
@@ -1,32 +1,63 @@
- $(function(){
-      SyntaxHighlighter.all();
-    });
-    $(window).load(function(){
-      $('.flexslider').flexslider({
-        animation: "slide",
-        start: function(slider){
-          $('body').removeClass('loading');
-        }
-      });
-    
- $('.flexslider2').flexslider({
-    animation: "slide",
-    animationLoop: false,
-    itemWidth: 210,
-    itemMargin: 5
-  });	
+// $(function(){
+//      SyntaxHighlighter.all();
+//    });
+//    $(window).load(function(){
+//      $('.flexslider').flexslider({
+//        animation: "slide",
+//        start: function(slider){
+//          $('body').removeClass('loading');
+//        }
+//      });
+//    
+// $('.flexslider2').flexslider({
+//    animation: "slide",
+//    animationLoop: false,
+//    itemWidth: 210,
+//    itemMargin: 5
+//  });	
+//	
+//});
 	
+	
+	
+$(document).ready(function() {
+    //Progres bar
+	var pbarvalue 
+	$(".progressbar001").each(function (index) {
+		pbarvalue = $(this).attr("data-pbpercent")
+		$(this).find("span:last-child").css("width",pbarvalue + "%")
+		$(this).find("span:nth-child(2)").append(pbarvalue+"%")
 	});
 	
+	//acordeon
+	$(".acordeon li a:first-child").click(function(){
+    	var idacordeon =  $(this).parent().parent().attr("id")
+		var itsopen = $(this).next("p").css("display")
+		if (itsopen=="none"){
+			$("#"+idacordeon+" li p").hide()
+			$("#"+idacordeon+" li a").removeClass("selected")
+			$(this).addClass("selected")
+			$(this).next("p").fadeIn()
+		}
+	});
+
+	//tabs
+	$(".tabs li").click(function(){
+		var idtab =  $(this).parent().parent().attr("id")
+		var showtab =  $(this).attr("data-content")
+		var itsopen = $("#"+idtab+ " .contentabs p."+showtab).css("display")
+
+		if (itsopen=="none"){
+			$("#"+idtab+" .tabs li").removeClass("selected")
+			$(this).addClass("selected")
+			$("#"+idtab+" .contentabs p").hide();
+			$("#"+idtab+ " .contentabs p."+showtab).fadeIn();
+		}
+	
+	})
 	
 	
-	
-	
-	
-	
-	
-	
-	
+});	
 	
 	
 var _gaq = _gaq || [];
